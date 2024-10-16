195542e0bdbf04b7a9e62d635b9242390ed078c2
Mai Trang <116065556+BanhMiRuoc@users.noreply.github.com>
2024-10-17 00:08:04 +0700

    fix the bug

--- lab05/src/main/java/org/example/lab05/Models/Product.java
+++ lab05/src/main/java/org/example/lab05/Models/Product.java
@@ -2,11 +2,7 @@ package org.example.lab05.Models;
 
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 
-@Setter
-@Getter
 @Entity
 @Table(name="products")
 public class Product {
@@ -25,6 +21,24 @@ public class Product {
 
     public Product() {
         super();
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
     }
 
     @Override
--- lab05/src/main/java/org/example/lab05/Models/User.java
+++ lab05/src/main/java/org/example/lab05/Models/User.java
@@ -1,11 +1,6 @@
 package org.example.lab05.Models;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
-@Setter
-@Getter
 @Entity
 @Table(name="users")
 
@@ -31,6 +26,31 @@ public class User {
 
     public User() {
         super();
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Override
