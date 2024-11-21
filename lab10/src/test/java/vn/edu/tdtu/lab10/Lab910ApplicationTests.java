d790cae04cefd044fdeac2a05b9c448be6e1bf79
Mai Trang <116065556+BanhMiRuoc@users.noreply.github.com>
2024-11-21 14:25:16 +0700

    upload lab10

---
+++ lab10/src/test/java/vn/edu/tdtu/lab10/Lab910ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.tdtu.lab10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
